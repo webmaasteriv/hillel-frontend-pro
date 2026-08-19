9f6c1c6af7b5e9a7d6fc821d0eba588b481ab480
webmaasteriv <webmaster.igor.voronkov@gmail.com>
2026-08-19 17:30:48 +0300

    Ready Home Work 16

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-### Home work for Hillel Front-end Pro course
+### Home work #16 for Hillel Front-end Pro course
+
+Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length. span>
--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const generateKey = (length, characters) => {
+    let i = 0;
+    let generatedKey = '';
+    while(i < length){
+        generatedKey += characters[Math.floor(Math.random()*characters.length)]
+        i++;
+    }
+    return generatedKey;
+}
+
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+const key = generateKey(16, characters);
+console.log(key);
