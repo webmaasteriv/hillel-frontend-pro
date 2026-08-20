0c131f07335d58ddd5a6f33b83452e39c975edc2
webmaasteriv <webmaster.igor.voronkov@gmail.com>
2026-08-20 16:56:35 +0300

    Ready home work 18

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-### Home work for Hillel Front-end Pro course
+### Home work #18 for Hillel Front-end Pro course
+
+Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+const removeElement = (array, position) =>{
+    if(array === undefined){
+        return 'Error: array undefined';
+    }
+    if(position === undefined){
+        return 'Error: position undefined';
+    }
+
+    array.splice(position - 1, 1);
+
+    return array;
+}
+
+removeElement(array, 5 );
+console.log(array); // Результат: [1, 2, 3, 4, 6, 7]
