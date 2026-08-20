efade8ec1942902588aff8294df76a7495c1eb69
webmaasteriv <webmaster.igor.voronkov@gmail.com>
2026-08-20 16:34:32 +0300

    Ready home work 17

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+const padString = (str, length, symbol = "*", position = true) => {
+        if(str === undefined){
+            return 'Error: string undefined';
+        }
+        if(length === undefined){
+            return 'Error: length undefined';
+        }
+        let symbols = '';
+        if(length > str.length){
+            let i = length;
+            while(i > str.length){
+                symbols += symbol;
+                i--;
+            }
+            if(position){
+                return str + symbols;
+            } else {
+                return symbols + str;
+            }
+
+        } else {
+            return str.substring(0, length);
+        }
+}
+
+console.log(padString('hello', 8, '*'));
+console.log(padString('hello', 6, '*', false));
+console.log(padString('hello', 2));
+console.log(padString('hello'));
+console.log(padString());
