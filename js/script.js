88f9dde7d4701ddf314820645c9138a992e15024
webmaasteriv <webmaster.igor.voronkov@gmail.com>
2026-09-21 13:12:44 +0300

    Ready Home Work #23

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const tree = document.querySelectorAll("#tree li");
+
+const texts = [];
+
+for (const li of tree) {
+    console.log(li);
+
+    texts.push(li.firstChild.textContent.trim());
+}
+
+console.log("Кількість елементів:", tree.length);
+
+console.log("Масив текстів:", texts);
